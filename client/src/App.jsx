fc35e9656bfa83b6717b2ace481db885f3a88eef
kost1k5 <kostyatv1790@gmail.com>
2026-02-16 09:13:16 +0300

    Initial commit: monorepo structure with client (React + Vite)

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import HelloWorld from './components/helloWorld'  
+
+
+function App() {
+  
+  return (
+    <div className='HellowWorld'>
+<HelloWorld />
+    </div>
+  )
+}
+
+export default App
